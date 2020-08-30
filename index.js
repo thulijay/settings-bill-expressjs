b4aef25170d7405611b49b4a0b025b280f94cc54
thulijay <thulijay79@gmail.com>
2020-08-31 00:35:00 +0200

    changed port number

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
-const express =require('express');
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 
-const settingsApp = express();
+const SettingsBill = require('./settings-bill');
+const settingsBill = SettingsBill();
 
-settingsApp.get('/', function(req, res){
-  res.send('Settings Bill App')
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.set('view engine', 'handlebars');
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json());
+
+app.get('/', function (req, res){
+	res.render('index',{
+		settings: settingsBill.getSettings(),
+		totals: settingsBill.totals()
+	});
 });
 
-const PORT = process.env.PORT || 3011;
+app.post('/settings', function (req, res){
+	settingsBill.setSettings({
+		callCost: req.body.callCost,
+		smsCost: req.body.smsCost,
+		warningLevel: req.body.warningLevel,
+		criticalLevel: req.body.criticalLevel
+	});
 
-settingsApp.listen(PORT, function(){
-  console.log('App started')
+	res.redirect('/');
+});
+
+app.post('/action', function (req, res){
+
+	settingsBill.recordAction(req.body.actionType)
+
+	res.redirect('/');
+});
+
+app.get('/actions', function (req, res){
+	res.render('actions',{
+		actions: settingsBill.actions()
+	});
+});
+
+app.get('/actions/:actionType', function (req, res){
+	const actionType = req.params.actionType;
+	res.render('actions', {
+		actions: settingsBill.actionsFor(actionType)
+	});
+});
+
+const PORT = process.env.PORT || 3012;
+app.listen(PORT, function (){
+	console.log('App started at:', PORT);
 });
