79cb66eae88445759b8e351634080efb884f3183
thulijay <thulijay79@gmail.com>
2020-08-31 01:34:41 +0200

    added travis badge

--- test/settings-bill-test.js
+++ test/settings-bill-test.js
@@ -13,15 +13,15 @@ describe('settings-bill', function(){
 
     it('should be able to set the settings', function(){
         settingsBill.setSettings({
-            smsCost: 2.35,
-            callCost: 3.35,
+            smsCost: 0.75,
+            callCost: 1.80,
             warningLevel: 30,
             criticalLevel: 40
         });
 
         assert.deepEqual({
-            smsCost: 2.35,
-            callCost: 3.35,
+            smsCost: 0.75,
+            callCost:1.80,
             warningLevel: 30,
             criticalLevel: 40
         }, settingsBill.getSettings())
@@ -32,8 +32,8 @@ describe('settings-bill', function(){
     it('should calculate the right totals', function(){
         const settingsBill = SettingsBill();
         settingsBill.setSettings({
-            smsCost: 2.35,
-            callCost: 3.35,
+            smsCost: 0.75,
+            callCost:1.80,
             warningLevel: 30,
             criticalLevel: 40
         });
@@ -41,17 +41,17 @@ describe('settings-bill', function(){
         settingsBill.recordAction('call');
         settingsBill.recordAction('sms');
 
-        assert.equal(2.35, settingsBill.totals().smsTotal);
-        assert.equal(3.35, settingsBill.totals().callTotal);
-        assert.equal(5.70, settingsBill.totals().grandTotal);
+        assert.equal(0.75, settingsBill.totals().smsTotal);
+        assert.equal(1.80, settingsBill.totals().callTotal);
+        assert.equal(2.55, settingsBill.totals().grandTotal);
 
     });
 
     it('should calculate the right totals for multiple actions', function(){
         const settingsBill = SettingsBill();
         settingsBill.setSettings({
-            smsCost: 2.35,
-            callCost: 3.35,
+            smsCost: 0.75,
+            callCost: 1.80,
             warningLevel: 30,
             criticalLevel: 40
         });
@@ -61,41 +61,41 @@ describe('settings-bill', function(){
         settingsBill.recordAction('sms');
         settingsBill.recordAction('sms');
 
-        assert.equal(4.70, settingsBill.totals().smsTotal);
-        assert.equal(6.70, settingsBill.totals().callTotal);
-        assert.equal(11.40, settingsBill.totals().grandTotal);
+        assert.equal(1.50, settingsBill.totals().smsTotal);
+        assert.equal(3.60, settingsBill.totals().callTotal);
+        assert.equal(5.10, settingsBill.totals().grandTotal);
 
     });
 
     it('should know when warning level reached', function(){
         const settingsBill = SettingsBill();
         settingsBill.setSettings({
-            smsCost: 2.50,
-            callCost: 5.00,
-            warningLevel: 5,
-            criticalLevel: 10
+            smsCost: 1.75,
+            callCost: 3.30,
+            warningLevel: 18,
+            criticalLevel: 13
         });
 
         settingsBill.recordAction('call');
         settingsBill.recordAction('sms');
 
-        assert.equal(true, settingsBill.hasReachedWarningLevel());
+        assert.equal(false, settingsBill.hasReachedWarningLevel());
     });
 
     it('should know when critical level reached', function(){
         const settingsBill = SettingsBill();
         settingsBill.setSettings({
-            smsCost: 2.50,
-            callCost: 5.00,
-            warningLevel: 5,
-            criticalLevel: 10
+            smsCost: 1.75,
+            callCost: 3.30,
+            warningLevel: 18,
+            criticalLevel: 13
         });
 
         settingsBill.recordAction('call');
         settingsBill.recordAction('call');
         settingsBill.recordAction('sms');
 
-        assert.equal(true, settingsBill.hasReachedCriticalLevel());
+        assert.equal(false, settingsBill.hasReachedCriticalLevel());
 
     });
 });
